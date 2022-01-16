1403d7900e8d0a1f52559ed6a9f4e257f495b2bc
orini <ori.nizri.tal@gmail.com>
2022-01-16 16:14:43 +0200

    heroku

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use("/users", bankRouter);
+app.use("/", bankRouter);
 
 const port = process.env.PORT || 3000 
 app.listen(port, ()=> {
