a7cd653c07721f630eca700a07cea40122be4e66
orini <ori.nizri.tal@gmail.com>
2022-01-16 18:36:26 +0200

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use("/users", bankRouter); //bankRouter
+app.use("/", bankRouter); //bankRouter
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=> {
